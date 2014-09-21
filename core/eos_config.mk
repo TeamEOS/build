1ee227f0c319f5520e5ddaa709d7e2f69ce5f021
anders3408 <anders3408@yahoo.dk>
2014-09-21 20:17:59 +0200

    build: Use ARCH_ARM_HIGH_OPTIMIZATION for additional CLFAG optimizing *Add -Os to thumb_CFLAGS (aosp default) *Only set -O2 or -O3 one time, from eos_config to avoid confusing *Remove lines per request from @Randall
    Change-Id: I8a1b3f5337a28365bab0c6f4a857fdb32d6c6599

--- core/eos_config.mk
+++ core/eos_config.mk
@@ -14,46 +14,15 @@
 # limitations under the License.
 #
 
-# Only set -O3 for thumb cflags if explicitly specified
+# Only set -O3 cflags if explicitly specified
 ifeq ($(ARCH_ARM_HIGH_OPTIMIZATION),true)
   TARGET_thumb_CFLAGS += -O3
+  TARGET_arm_CFLAGS += -O3
+  $(combo_target)RELEASE_CFLAGS += -O3
 else
   TARGET_thumb_CFLAGS += -Os
-endif
-
-# Only set -O3 for arm cflags if explicitly specified
-ifeq ($(ARCH_ARM_HIGH_OPTIMIZATION),true)
-  TARGET_arm_CFLAGS += -O3
-else
-  TARGET_arm_CFLAGS += -Os
-endif
-
-# A clean way of only disabling a few optimizations that
-# cause problems on devices such as Grouper
-ifeq ($(ARCH_ARM_HIGH_OPTIMIZATION_COMPAT),true)
-  TARGET_EOS_CFLAGS += -fno-tree-vectorize
-endif
-
-# Use -O2 for TARGET_arm_CFLAGS for the following devices due to
-# "problem" modules until they're discovered
-ifeq ($(ARCH_ARM_HIGH_OPTIMIZATION),true)
-ifneq ($(filter m7att m7spr m7tmo m7wls,$(PRODUCT_DEVICE)),)
   TARGET_arm_CFLAGS += -O2
-else
-ifeq ($(TARGET_BUILD_SMALL_SYSTEM),true)
-    TARGET_arm_CFLAGS += -O2
-else
-    TARGET_arm_CFLAGS += -O3
-endif
-endif
-endif
-
-ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),legacy)
-  TARGET_EOS_CFLAGS += -DLEGACY_QCOM_VARIANT
-endif
-
-ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf)
-  TARGET_EOS_CFLAGS += -DCAF_QCOM_VARIANT
+  $(combo_target)RELEASE_CFLAGS += -O2
 endif
 
 # Global defines for skia neon optimization
