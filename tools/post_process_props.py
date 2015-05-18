5d12826e687890b97b09441a8886f386f531e2d7
Randall Rushing <randall.rushing@gmail.com>
2015-05-18 00:39:01 -0700

    build: tighten up secure adb handling
    Looks like it had been disabled by default here. Bring it back
    and let our vendor disable it as needed
    
    Change-Id: I8e977e0cb1ea49f203ce97c46e4866b33eaf8a29

--- tools/post_process_props.py
+++ tools/post_process_props.py
@@ -40,9 +40,9 @@ def mangle_build_prop(prop, overrides):
 # Put the modifications that you need to make into the /default.prop into this
 # function. The prop object has get(name) and put(name,value) methods.
 def mangle_default_prop(prop):
-  # If ro.debuggable is 1, then enable adb on USB by default
-  # (this is for userdebug builds)
-  if prop.get("ro.debuggable") == "1":
+  # If ro.adb.secure is not 1, then enable adb on USB by default
+  # (this is for eng builds)
+  if prop.get("ro.adb.secure") != "1":
     val = prop.get("persist.sys.usb.config")
     if val == "":
       val = "adb"
