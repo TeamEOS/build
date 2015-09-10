LineageOS/android_build

build/core: Allow EXTERNAL_CLEAN_TARGETS to dump vars.
Change-Id: I4623ad38959aaeac75d6b7905133562a195ef531

core/dumpvar.mk
@@ -84,8 +84,10 @@ $(info   HOST_OS_EXTRA=$(HOST_OS_EXTRA))
 $(info   HOST_BUILD_TYPE=$(HOST_BUILD_TYPE))
 $(info   BUILD_ID=$(BUILD_ID))
 $(info   OUT_DIR=$(OUT_DIR))
-ifeq ($(CYNGN_TARGET),true)
+ifneq (,$(filter true, $(CYNGN_TARGET) $(EXTERNAL_CLEAN_TARGET)))
+ifeq ($(CYNGN_TARGET), true)
 $(info   CYNGN_TARGET=$(CYNGN_TARGET))
+endif
 $(info   CYNGN_FEATURES=$(CYNGN_FEATURES))
 endif
 $(info ============================================)

TeamEOS/build

Conflicts:
	core/dumpvar.mk

core/dumpvar.mk
@@ -84,8 +84,11 @@ $(info   HOST_OS_EXTRA=$(HOST_OS_EXTRA))
 $(info   HOST_BUILD_TYPE=$(HOST_BUILD_TYPE))
 $(info   BUILD_ID=$(BUILD_ID))
 $(info   OUT_DIR=$(OUT_DIR))
-ifeq ($(EOSPRIV_TARGET),true)
+
+ifneq (,$(filter true, $(EOSPRIV_TARGET) $(EXTERNAL_CLEAN_TARGET)))
+ifeq ($(EOSPRIV_TARGET), true)
 $(info   EOSPRIV_TARGET=$(EOSPRIV_TARGET))
+endif
 $(info   EOSPRIV_FEATURES=$(EOSPRIV_FEATURES))
 endif
 $(info ============================================)